7b2753dbad6ab9cc46092b834c617ce7f3c7f2c4
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2022-12-12 00:00:21 +0000

    Create Vit_multiheadedAttention.py
    Vit model for image classification task

---
+++ Vit_multiheadedAttention.py
@@ -0,0 +1,191 @@
+# This implementation is identical to that given by https://github.com/alessandrolamberti/ViT. Yet, under this example, we aggregate everything into a single file for an easy implementation.
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor, nn
+from torchsummary import summary
+from torchvision.transforms import Compose, Resize, ToTensor
+
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange, Reduce
+
+img = Image.open(r"..\cats_dogs_pandas\cat100.jpg")
+#img_size = 224
+fig = plt.figure()
+plt.imshow(img)
+plt.show()
+
+transform = Compose([
+    Resize((224, 224)),
+    ToTensor(),
+])
+
+x = transform(img)
+x = x.unsqueeze(0)
+print(x.shape)
+
+patch_size = 16
+patches = rearrange(x, 'b c (h s1) (w s2) -> b (h w) (s1 s2 c)', s1=patch_size, s2=patch_size)
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 224):
+        self.patch_size = patch_size
+        super().__init__()
+        self.projection = nn.Sequential(
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b e (h) (w) -> b (h w) e'),
+        ) # this breaks down the image in s1xs2 patches, and then flat them
+        
+        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
+        self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
+
+        
+    def forward(self, x: Tensor) -> Tensor:
+        b, _, _, _ = x.shape
+        x = self.projection(x)
+        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
+        x = torch.cat([cls_tokens, x], dim=1) #prepending the cls token
+        x += self.positions
+        return x
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.qkv = nn.Linear(emb_size, emb_size * 3) # queries, keys and values matrix
+        self.att_drop = nn.Dropout(dropout)
+        self.projection = nn.Linear(emb_size, emb_size)
+        
+    def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
+        # split keys, queries and values in num_heads
+        qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
+        queries, keys, values = qkv[0], qkv[1], qkv[2]
+        # sum up over the last axis
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys) # batch, num_heads, query_len, key_len
+        
+        if mask is not None:
+            fill_value = torch.finfo(torch.float32).min
+            energy.mask_fill(~mask, fill_value)
+            
+        scaling = self.emb_size ** (1/2)
+        
+        att = F.softmax(energy, dim=-1) / scaling
+        att = self.att_drop(att)
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values) # sum over the third axis
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.projection(out)
+        
+        return out
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.qkv = nn.Linear(emb_size, emb_size * 3) # queries, keys and values matrix
+        self.att_drop = nn.Dropout(dropout)
+        self.projection = nn.Linear(emb_size, emb_size)
+        
+    def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
+        # split keys, queries and values in num_heads
+        qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
+        queries, keys, values = qkv[0], qkv[1], qkv[2]
+        # sum up over the last axis
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys) # batch, num_heads, query_len, key_len
+        
+        if mask is not None:
+            fill_value = torch.finfo(torch.float32).min
+            energy.mask_fill(~mask, fill_value)
+            
+        scaling = self.emb_size ** (1/2)
+        
+        att = F.softmax(energy, dim=-1) / scaling
+        att = self.att_drop(att)
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values) # sum over the third axis
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.projection(out)
+        
+        return out
+    
+class ResidualAdd(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+        
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return x
+    
+    
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self, emb_size: int, L: int = 4, drop_p: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, L * emb_size),
+            nn.GELU(),
+            nn.Dropout(drop_p),
+            nn.Linear(L * emb_size, emb_size),
+        )
+        
+
+class TransformerEncoderBlock(nn.Sequential):
+    def __init__(self, emb_size: int = 768, drop_p: float = 0., forward_expansion: int = 4,
+                 forward_drop_p: float = 0.,
+                 **kwargs):
+                 
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size, **kwargs),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                FeedForwardBlock(
+                    emb_size, L=forward_expansion, drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            )
+            ))
+        
+        
+patches_embedded = PatchEmbedding()(x)
+print(TransformerEncoderBlock()(patches_embedded).shape)
+
+class TransformerEncoder(nn.Sequential):
+    def __init__(self, depth: int = 12, **kwargs):
+        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
+        
+        
+        
+        
+        
+class ClassificationHead(nn.Sequential):
+    def __init__(self, emb_size: int = 768, n_classes: int = 1000):
+        super().__init__(
+            Reduce('b n e -> b e', reduction='mean'),
+            nn.LayerNorm(emb_size), 
+            nn.Linear(emb_size, n_classes))
+        
+class ViT(nn.Sequential):
+    def __init__(self,     
+                in_channels: int = 3,
+                patch_size: int = 16,
+                emb_size: int = 768,
+                img_size: int = 224,
+                depth: int = 12,
+                n_classes: int = 1000,
+                **kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
+            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
+            ClassificationHead(emb_size, n_classes)
+        )
+        
+model =  ViT()
+summary(model, (3,224,224))
