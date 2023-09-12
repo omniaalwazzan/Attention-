2a939ccce5e80240392683dc168cf05e5c914d4f
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-09-12 22:56:56 +0100

    Create trans_block.py

---
+++ trans_block.py
@@ -0,0 +1,108 @@
+import os
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+import torch.nn as nn
+
+NUM_WORKERS = os.cpu_count()
+
+# 1. Create a class that inherits from nn.Module
+class MultiheadSelfAttentionBlock(nn.Module):
+    """Creates a multi-head self-attention block ("MSA block" for short).
+    """
+    # 2. Initialize the class with hyperparameters from Table 1
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                 num_heads:int=12, # Heads from Table 1 for ViT-Base
+                 attn_dropout:float=0): # doesn't look like the paper uses any dropout in MSABlocks
+        super().__init__()
+        
+        # 3. Create the Norm layer (LN)
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        # 4. Create the Multi-Head Attention (MSA) layer
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=embedding_dim,
+                                                    num_heads=num_heads,
+                                                    dropout=attn_dropout,
+                                                    batch_first=True) # does our batch dimension come first?
+        
+    # 5. Create a forward() method to pass the data throguh the layers
+    def forward(self, x):
+        x = self.layer_norm(x)
+        attn_output, _ = self.multihead_attn(query=x, # query embeddings 
+                                             key=x, # key embeddings
+                                             value=x, # value embeddings
+                                             need_weights=False) # do we need the weights or just the layer outputs?
+        return attn_output 
+#1. Create a class that inherits from nn.Module
+class MLPBlock(nn.Module):
+    """Creates a layer normalized multilayer perceptron block ("MLP block" for short)."""
+    # 2. Initialize the class with hyperparameters from Table 1 and Table 3
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden Size D from Table 1 for ViT-Base
+                 mlp_size:int=3072, # MLP size from Table 1 for ViT-Base
+                 dropout:float=0.1): # Dropout from Table 3 for ViT-Base
+        super().__init__()
+        
+        # 3. Create the Norm layer (LN)
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        # 4. Create the Multilayer perceptron (MLP) layer(s)
+        self.mlp = nn.Sequential(
+            nn.Linear(in_features=embedding_dim,
+                      out_features=mlp_size),
+            nn.GELU(), # "The MLP contains two layers with a GELU non-linearity (section 3.1)."
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=mlp_size, # needs to take same in_features as out_features of layer above
+                      out_features=embedding_dim), # take back to embedding_dim
+            nn.Dropout(p=dropout) # "Dropout, when used, is applied after every dense layer.."
+        )
+    
+    # 5. Create a forward() method to pass the data throguh the layers
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = self.mlp(x)
+        return x
+# 1. Create a class that inherits from nn.Module
+class TransformerEncoderBlock(nn.Module):
+    """Creates a Transformer Encoder block."""
+    # 2. Initialize the class with hyperparameters from Table 1 and Table 3
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                 num_heads:int=12, # Heads from Table 1 for ViT-Base
+                 mlp_size:int=3072, # MLP size from Table 1 for ViT-Base
+                 mlp_dropout:float=0.1, # Amount of dropout for dense layers from Table 3 for ViT-Base
+                 attn_dropout:float=0): # Amount of dropout for attention layers
+        super().__init__()
+
+        # 3. Create MSA block (equation 2)
+        self.msa_block = MultiheadSelfAttentionBlock(embedding_dim=embedding_dim,
+                                                     num_heads=num_heads,
+                                                     attn_dropout=attn_dropout)
+        
+        # 4. Create MLP block (equation 3)
+        self.mlp_block =  MLPBlock(embedding_dim=embedding_dim,
+                                   mlp_size=mlp_size,
+                                   dropout=mlp_dropout)
+        
+    # 5. Create a forward() method  
+    def forward(self, x):
+        
+        # 6. Create residual connection for MSA block (add the input to the output)
+        x =  self.msa_block(x) + x 
+        
+        # 7. Create residual connection for MLP block (add the input to the output)
+        x = self.mlp_block(x) + x 
+        
+        return x
+    
+transformer_encoder_block = TransformerEncoderBlock()
+
+from torchinfo import summary
+# # Print an input and output summary of our Transformer Encoder (uncomment for full output)
+summary(model=transformer_encoder_block,
+        input_size=(1, 197, 768), # (batch_size, num_patches, embedding_dimension)
+        col_names=["input_size", "output_size", "num_params", "trainable"],
+        col_width=20,
+       row_settings=["var_names"])
